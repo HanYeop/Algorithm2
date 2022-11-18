beff196826724681ec8dacadc67a96f8b97adce0
Sangyeop Han <50766393+HanYeop@users.noreply.github.com>
2022-11-19 00:39:58 +0900

    [프로그래머스] 전화번호 목록 (Java)

---
+++ 구현/전화번호 목록.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+// [프로그래머스] 전화번호 목록 (Java)
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Arrays.sort(phone_book);       
+
+        for(int i=0; i < phone_book.length; i++){
+            if(i == phone_book.length -1) break;
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                answer = false;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
